4702bf1566133b873e3fca2061925efa66dc96bb
Vpsush <vpsush2023@gmail.com>
2023-08-01 22:19:32 +0300

    fvxc

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,32 @@
+import throttle from 'lodash.throttle';
 
+const options = {
+  id: 59777392,
+  width: 640,
+  loop: true,
+};
+
+const player = new Vimeo.Player('made-in-ny', options);
+
+player.setVolume(0);
+
+player.on('play', function () {
+  console.log('played the video!');
+});
+
+player
+  .setCurrentTime(30.456)
+  .then(function (seconds) {
+    // seconds = the actual time that the player seeked to
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        // the time was less than 0 or greater than the video’s duration
+        break;
+
+      default:
+        // some other error occurred
+        break;
+    }
+  });
