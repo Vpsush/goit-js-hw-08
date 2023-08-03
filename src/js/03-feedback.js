580712827230398013d6ff72d1e4660ef1abb8dd
Vpsush <vpsush2023@gmail.com>
2023-08-03 23:34:04 +0300

    vcfc

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,22 @@
+import '../css/common.css';
+import '../css/03-feedback.css';
+// import throttle from 'lodash.throttle';
 
+const storageKey = 'feedback-form-state';
+
+const refs = {
+  form: document.querySelector('.feedback-form'),
+  textarea: document.querySelector('.feedback-form textarea'),
+};
+
+refs.form.addEventListener('submit', onFormSubmit);
+refs.textarea.addEventListener('input', onTextAreaSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+  event.currentTarget.reset();
+}
+function onTextAreaSubmit(event) {
+  const message = event.currentTarget.value;
+  localStorage.setItem(storageKey, message);
+}
