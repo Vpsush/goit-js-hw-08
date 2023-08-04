4e3868918962f14f389bad1940e14b39908851a3
Vpsush <130247541+Vpsush@users.noreply.github.com>
2023-08-04 16:49:38 +0000

    Deploying to gh-pages from @ Vpsush/goit-js-hw-08@8d733d4f172835776c0da84dcb00feb62849d616 🚀

--- 03-feedback.74b45437.js
+++
@@ -1,2 +0,0 @@
-const e={form:document.querySelector(".feedback-form"),textarea:document.querySelector(".feedback-form textarea")};e.form.addEventListener("submit",(function(e){e.preventDefault(),e.currentTarget.reset()})),e.textarea.addEventListener("input",(function(e){const t=e.currentTarget.value;localStorage.setItem("feedback-form-state",t)}));
-//# sourceMappingURL=03-feedback.74b45437.js.map
--- 03-feedback.7bdb0348.js
+++
@@ -1,2 +0,0 @@
-!function(){var e={form:document.querySelector(".feedback-form"),textarea:document.querySelector(".feedback-form textarea")};e.form.addEventListener("submit",(function(e){e.preventDefault(),e.currentTarget.reset()})),e.textarea.addEventListener("input",(function(e){var t=e.currentTarget.value;localStorage.setItem("feedback-form-state",t)}))}();
-//# sourceMappingURL=03-feedback.7bdb0348.js.map
