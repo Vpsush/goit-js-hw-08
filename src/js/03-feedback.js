afcc70eef2897567f127355c4ff331d4736f95f3
Vpsush <vpsush2023@gmail.com>
2023-08-08 22:17:41 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,9 +1,8 @@
-// import '../css/common.css';
-// import '../css/03-feedback.css';
 import throttle from 'lodash.throttle';
 
 const storageKey = 'feedback-form-state';
-const formData = {};
+const formData = JSON.parse(localStorage.getItem(storageKey)) || {};
+console.log(formData);
 
 const refs = {
   form: document.querySelector('.feedback-form'),
@@ -13,33 +12,12 @@ const refs = {
 refs.form.addEventListener('submit', onFormSubmit);
 refs.form.addEventListener('input', throttle(onTextAreaSubmit, 500));
 
-refs.form.addEventListener('input', e => {
-  formData[e.target.name] = e.target.value;
-  console.log(formData);
-  localStorage.setItem(storageKey, JSON.stringify(formData));
-  // console.log(e.target.name);
-  // console.log(e.target.value);
-});
-
-function onFormSubmit(event) {
-  event.preventDefault();
-  event.currentTarget.reset();
+function onFormSubmit(e) {
+  e.preventDefault();
+  e.currentTarget.reset();
   localStorage.removeItem(storageKey);
 }
-function onTextAreaSubmit(event) {
-  const message = event.target.value;
-  localStorage.setItem(storageKey, message);
+function onTextAreaSubmit(e) {
+  formData[e.target.name] = e.target.value;
+  localStorage.setItem(storageKey, JSON.stringify(formData));
 }
-function populateTextarea() {
-  const savedMessage = localStorage.getItem(storageKey);
-  if (savedMessage) {
-    return;
-  }
-  // refs.textarea.value = savedMessage;
-  refs.textarea.value = savedMessage.message || '';
-  refs.input.value = savedMessage.email || '';
-  formData.email = savedMessage.email || '';
-  formData.message = savedMessage.message || '';
-}
-
-populateTextarea();
